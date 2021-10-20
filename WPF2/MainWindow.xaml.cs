c2405e06100c64020d6ffebd64a574282c08d378
Lucas Juan <ljgsodre@outlook.com>
2021-10-20 15:22:51 -0300

    Adicionar arquivos de projeto.

---
+++ WPF2/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using WPF2.Model;
+
+namespace WPF2
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Vendedor> lista = new List<Vendedor>();
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Vendedor V = new Vendedor();
+            int codigo = 0;
+            double TotalVendas = 0;
+            Int32.TryParse(txtcodigo.Text, out codigo);
+            Double.TryParse(txttotalVendas.Text, out TotalVendas);
+            V.codigo = codigo;
+            V.nome = txtnome.Text;
+            V.TotalVendas = TotalVendas;
+            //validar os campos
+            if (!V.ValidarCampos())
+            {
+                MessageBox.Show("Os campos devem ser preenchidos corretamente");
+                return;
+            }
+
+            if (lista.Count > 0)
+            {
+                int? index = null;
+                foreach (var item in lista)
+                {
+                 
+                    //Verifica se a lista está preenchida e estou editando neste caso são dados que já existem
+                    if (item.codigo == V.codigo)
+                    {
+                        V.codigo = codigo;
+                        V.nome = txtnome.Text;
+                        V.TotalVendas = TotalVendas;
+                        V.CalculaComissao(V.TotalVendas);
+                        index = lista.IndexOf(item);
+                    }
+                }
+                if (index != null)
+                {
+                    lista.RemoveAt((Int32)index);
+                }
+
+                V.CalculaComissao(TotalVendas);
+                lista.Add(V);
+
+            }
+            else
+            {
+                V.CalculaComissao(TotalVendas);
+                lista.Add(V);
+            }
+            //Adiciona a Lista
+            dg.ItemsSource = lista;
+            dg.Items.Refresh();
+            dg.UpdateLayout();
+            limparCampos();
+
+        }
+
+        private void btnAlterar_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var data in dg.SelectedItems)
+            {
+                Vendedor d = data as Vendedor;
+                txtcodigo.Text = d.codigo.ToString();
+                txtnome.Text = d.nome.ToString();
+                txttotalVendas.Text = d.TotalVendas.ToString();
+            }
+        }
+
+        private void btnRemover_Click(object sender, RoutedEventArgs e)
+        {
+            int? index = null;
+            foreach (var data2 in dg.SelectedItems)
+            {
+                Vendedor d = data2 as Vendedor;
+                foreach (var item in lista)
+                {
+                    if (d.codigo == item.codigo)
+                        index = lista.IndexOf(item);
+                }
+            }
+            if(index != null)
+            {
+                lista.RemoveAt((Int32)index);
+            }
+            dg.Items.Refresh();
+            dg.UpdateLayout();
+        }
+        public void limparCampos()
+        {
+            txtnome.Text = "";
+            txtcodigo.Text = "";
+            txttotalVendas.Text = "";
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ WPF2/Model/Vendedor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF2.Model
+{
+    class Vendedor
+    {
+        public int codigo { get; set; }
+        public string nome { get; set; }
+        public double TotalVendas { get; set; }
+        public double ValorComissao { get; set; }
+
+        public void CalculaComissao(double TotalVendas)
+        {
+            if(TotalVendas >= (double)1000 && TotalVendas <= (double)5000)
+            {
+                ValorComissao = 0.03*TotalVendas;
+            }
+            if (TotalVendas > (double)5000 && TotalVendas <= (double) 10000)
+            {
+                ValorComissao = 0.07* TotalVendas;
+            }
+            if (TotalVendas > (double) 10000)
+            {
+                ValorComissao = 0.12*TotalVendas;
+            }
+        }
+        public bool ValidarCampos()
+        {
+            if (codigo <= 0)
+                return false;
+            if (String.IsNullOrEmpty(nome))
+                return false;
+            if (TotalVendas < 0)
+                return false;
+
+            return true;
+        }
+    }
+}
